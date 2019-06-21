9a9e3699d68febf37541f2fe3a19f473daa89699
Gonzalo Cercos Vazquez <goncholo@gmail.com>
2019-06-21 16:00:08 +0200

    First commit tutorial branching and pull request

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4NetExample
+{
+    class Program
+    {
+
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger
+        (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            log.Info("This is an info log");
+            log.Error("This is my error");
+
+            Console.ReadLine();
+        }
+    }
+}
